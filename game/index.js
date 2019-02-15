bd57e81a6bb5064a4f3de614dedcce1c47aa5e26
Melanie <timdog563@gmail.com>
2019-02-14 18:39:27 -0700

    Got a circle displaying on the canvas

--- game/index.js
+++ game/index.js
@@ -1,3 +1,10 @@
+
+
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
 var app = new PIXI.Application({width:800, height:600, antialias: true, transparent:false, resolution:1});
 
 app.renderer.backgroundColor = 0x061639;
@@ -8,15 +15,24 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(app.view);
 
-var shittyfuckinimage = PIXI.Sprite.fromImage("b84.png");
+// loader.add("http://media.ifunny.com/results/2018/02/08/hcrbkca84w.jpg").load(setup);
 
-shittyfuckinimage.anchor.set(0.5);
+// function setup(){
+//     let shittyfuckinimage = new Sprite(resources["http://media.ifunny.com/results/2018/02/08/hcrbkca84w.jpg"].texture);
+// //    .fromImage("http://media.ifunny.com/results/2018/02/08/hcrbkca84w.jpg", true);
 
-shittyfuckinimage.x = app.screen.width /2;
-shittyfuckinimage.y = app.screen.height / 2;
+// }
 
-app.stage.addChild(shittyfuckinimage);
+let circle = new PIXI.Graphics;
+circle.lineStyle(40, 0xFF0000);
+circle.drawCircle(innerWidth / 2, innerHeight / 2, 20);
+circle.endFill();
+app.stage.addChild(circle);
 
-app.ticker.add(function(delta){
-    shittyfuckinimage.rotation += 0.1 *delta;
+
+const ticker = new PIXI.ticker.Ticker();
+ticker.stop();
+ticker.add((deltaTime) =>{
+    circle.y += 5 * deltaTime;
 });
+ticker.start();

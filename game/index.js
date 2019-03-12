ee2db5b91d8ec8b5ead3da6e7ddbbee6bbe69aaa
Melanie <timdog563@gmail.com>
2019-03-11 21:00:36 -0600

    I DID GOOD

--- game/index.js
+++ game/index.js
@@ -5,14 +5,14 @@ var socket = io();
 let isGame = false;
 let thisPlayerName = null;
 let currentGameState = null;
-let topX = 50;
-let topY = 25;
+let topX = 125;
+let topY = 0;
 let leftX = 25;
 let leftY = 50;
-let rightX = 75;
+let rightX = 225;
 let rightY = 50;
-let bottomX = 50;
-let bottomY = 75;
+let bottomX = 125;
+let bottomY = 100;
 
 function giveUsername(){
   var formy = document.getElementById('usernameFormy');
@@ -43,6 +43,7 @@ socket.on('gameStart', function(gameState){
   console.log(gameState);
   currentGameState = gameState;
   toggle();
+  changeButtonNames();
   drawFields();
 });
 
@@ -74,37 +75,54 @@ function drawFields(){
       let canvas = document.getElementById(`field${i+1}`);
       let context = canvas.getContext('2d');
       for(let j = 0; j < currentGameState.fields[i].chars.length; j++){
-        var heck = j+currentGameState.fields[i].leadIndex % 4;
+        var heck = (j+currentGameState.fields[i].leadIndex) % 4;
+        console.log(heck);
+        console.log(currentGameState.fields[i].chars[heck].imageUrl);
+        let profileImage = null;
         switch(j){
           case 0:
             profileImage = new Image();
             profileImage.src = currentGameState.fields[i].chars[heck].imageUrl;
             profileImage.onload = () =>{
-                context.drawImage(profileImage, rightX, rightY, 100, 100);
+                context.drawImage(profileImage, rightX, rightY, 50, 50);
+                console.log("boi");
             }
             break;
           case 1:
             profileImage = new Image();
             profileImage.src = currentGameState.fields[i].chars[heck].imageUrl;
             profileImage.onload = () =>{
-                context.drawImage(profileImage, bottomX, bottomY, 100, 100);
+                context.drawImage(profileImage, bottomX, bottomY, 50, 50);
+                console.log("do i");
             }
             break;
           case 2:
             profileImage = new Image();
             profileImage.src = currentGameState.fields[i].chars[heck].imageUrl;
             profileImage.onload = () =>{
-                context.drawImage(profileImage, leftX, leftY, 100, 100);
+                context.drawImage(profileImage, leftX, leftY, 50, 50);
+                console.log("hate");
             }
             break;
           case 3:
             profileImage = new Image();
             profileImage.src = currentGameState.fields[i].chars[heck].imageUrl;
             profileImage.onload = () =>{
-                context.drawImage(profileImage, topX, topY, 100, 100);
+                context.drawImage(profileImage, topX, topY, 50, 50);
+                console.log("life");
             }
             break;
         }
+      }
+    }
+  }
+}
+
+function changeButtonNames(){
+  for(let i = 0; i < 2; i++){
+    if(currentGameState.fields[i].ownerUsername === thisPlayerName){
+      for(let j = 0; j < currentGameState.fields[i].chars[currentGameState.fields[i].leadIndex].moves.length; j++){
+        document.getElementById(`move${j+1}`).innerHTML = currentGameState.fields[i].chars[currentGameState.fields[i].leadIndex].moves[j].Name;
       }
     }
   }
